38f0de61cfa1d76cd01bafbf9d363ea28f95138f
CYA <3155414334@qq.com>
2025-10-11 11:04:52 +0800

    feat:采用邮箱而非互斥锁保障线程安全

--- main/user_imu/user_imu.c
+++ main/user_imu/user_imu.c
@@ -11,9 +11,12 @@
 static const char* TAG = "from -> "__FILE__;
 
 static mpu6050_handle_t mpu6050_dev = NULL;
-static mpu6050_acce_value_t s_acce = {0};
-static mpu6050_gyro_value_t s_gyro = {0};
-static SemaphoreHandle_t imu_mutex = NULL;
+static QueueHandle_t imu_mailbox = NULL;
+
+typedef struct {
+    mpu6050_acce_value_t acce_value;
+    mpu6050_gyro_value_t gyro_value;
+} imu_data_t;
 
 /* 外界根据原始数据计算 roll 与 pitch */
 void imu_get_rp( mpu6050_acce_value_t *const acce_value, mpu6050_gyro_value_t *const gyro_value, complimentary_angle_t *const complimentary_angle)
@@ -24,19 +27,18 @@ void imu_get_rp( mpu6050_acce_value_t *const acce_value, mpu6050_gyro_value_t *c
 /* 外界读取加速度与陀螺仪的原始数据 */
 void imu_get_data( mpu6050_acce_value_t *const acce_value, mpu6050_gyro_value_t *const gyro_value)
 {
-    xSemaphoreTake( imu_mutex, portMAX_DELAY);
-    memcpy( acce_value, &s_acce, sizeof( mpu6050_acce_value_t ));
-    memcpy( gyro_value, &s_gyro, sizeof( mpu6050_gyro_value_t ));
-    xSemaphoreGive( imu_mutex );
+    imu_data_t imu_data = {0};
+    xQueuePeek( imu_mailbox, &imu_data, portMAX_DELAY);
+    memcpy( acce_value, &( imu_data.acce_value ), sizeof( mpu6050_acce_value_t ));
+    memcpy( gyro_value, &( imu_data.gyro_value ), sizeof( mpu6050_gyro_value_t ));
 }
 
 /* 读取加速度与陀螺仪的原始数据 */
 static void mpu6050_read( mpu6050_acce_value_t *const acce_value, mpu6050_gyro_value_t *const gyro_value)
 {
-    xSemaphoreTake( imu_mutex, portMAX_DELAY);
     mpu6050_get_acce(mpu6050_dev, acce_value);
     mpu6050_get_gyro(mpu6050_dev, gyro_value);
-    xSemaphoreGive( imu_mutex );
+
 }
 
 void imu_task(void* param)
@@ -45,17 +47,21 @@ void imu_task(void* param)
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
     mpu6050_handle_t* dev = (mpu6050_handle_t*)param;
+    imu_data_t imu_data = {0};
     while(1)
     {
-        mpu6050_read( &s_acce, &s_gyro);
-        ESP_LOGI(TAG, "ax=%f,ay=%f,az=%f,gx=%f,gy=%f,gz=%f", s_acce.acce_x,s_acce.acce_y,s_acce.acce_z,s_gyro.gyro_x,s_gyro.gyro_y,s_gyro.gyro_z);
+        mpu6050_read( &(imu_data.acce_value), &(imu_data.gyro_value) );
+        xQueueOverwrite( imu_mailbox, &imu_data);
+        ESP_LOGD(TAG, "ax=%f,ay=%f,az=%f,gx=%f,gy=%f,gz=%f", 
+            imu_data.acce_value.acce_x, imu_data.acce_value.acce_y, imu_data.acce_value.acce_z,
+            imu_data.gyro_value.gyro_x, imu_data.gyro_value.gyro_y, imu_data.gyro_value.gyro_z);
         vTaskDelayUntil(&xLastWakeTime, xDelay);
     }
 
     mpu6050_delete( *dev );
     *dev = NULL;
-    vSemaphoreDelete(imu_mutex);
-    imu_mutex = NULL;
+    vQueueDelete(imu_mailbox);
+    imu_mailbox = NULL;
     vTaskDelete(NULL);
 }
 
@@ -70,7 +76,9 @@ void imu_init(void)
     mpu6050_config(mpu6050_dev, ACCE_FS_2G, GYRO_FS_500DPS);
     mpu6050_wake_up(mpu6050_dev);
 
-    imu_mutex = xSemaphoreCreateMutex();
+    imu_mailbox = xQueueCreate( 1, sizeof(imu_data_t) );
     xTaskCreatePinnedToCore( imu_task, "imu_task", 4096, &mpu6050_dev, IMU_TASK_PRIO, NULL, 1);
+
+    ESP_LOGI(TAG, "IMU 初始化完毕!!!");
 }
 
