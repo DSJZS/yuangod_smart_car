4a97a1c6e839624ea20cd0f58486fc7f3ea07331
CYA <3155414334@qq.com>
2025-10-22 20:44:04 +0800

    test:添加了一些测试代码

--- lidar_esp32/main/app_main.c
+++ lidar_esp32/main/app_main.c
@@ -3,9 +3,13 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "wifi_station.h"
+
 void app_main(void)
 {
     ESP_LOGI("TAG", "hello world!!");
+
+    wifi_station_init();
     while(1)
     {
         vTaskDelay(1);
