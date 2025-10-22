e618ef4cfb825fe1a2d0202ff8fd992370e3a3cd
CYA <3155414334@qq.com>
2025-10-22 17:50:15 +0800

    feat:新增创建TCP转虚拟串口的bash脚本
    端口8899 转 /dev/ttyVIRT0 波特率115200用于连接底盘
    端口8888 转 /dev/ttyVIRT1 波特率115200用于连接雷达

---
+++ ros2_jazzy/close_tcp2vsp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash #脚本解释器为shell
+
+# 关闭所有 socat 进程
+sudo pkill socat
+
---
+++ ros2_jazzy/create_tcp2vsp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash #脚本解释器为shell
+
+# 创建虚拟串口并监听TCP
+# 如果想要检查是否创建成功，测试方式可以参考README.md相关说明
+# 1.底盘
+sudo sh -c 'socat -d -d PTY,link=/dev/ttyVIRT0,b115200,raw,echo=0,group=dialout,mode=660 TCP-LISTEN:8899,reuseaddr &'
+# 2.雷达
+sudo sh -c 'socat -d -d PTY,link=/dev/ttyVIRT1,b115200,raw,echo=0,group=dialout,mode=660 TCP-LISTEN:8888,fork,reuseaddr &'
