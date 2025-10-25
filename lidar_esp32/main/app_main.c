cab620222000778c153f53e8d1eca991ddbfd4c5
CYA <3155414334@qq.com>
2025-10-25 13:09:23 +0800

    docs:添加了main函数中的注释, 说明一些步骤的功能

--- lidar_esp32/main/app_main.c
+++ lidar_esp32/main/app_main.c
@@ -11,14 +11,10 @@ void app_main(void)
 {
     ESP_LOGI("TAG", "hello world!!");
 
-    // wifi站点模式初始化用于连接底盘上的ESP32 AP
+    /* wifi站点模式初始化用于连接底盘上的ESP32 AP */
     wifi_station_init();
+    /* 创建连接ROS2服务器的socket */
     socket_ros2_config();
-
+    /* 初始化连接LD14的串口并创建一个任务定时转发雷达数据到ROS2服务器 */
     ld14_init();
-
-    while(1)
-    {
-        vTaskDelay(1);
-    }
 }
