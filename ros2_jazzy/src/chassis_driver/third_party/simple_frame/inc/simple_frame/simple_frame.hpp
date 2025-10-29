881b2d6edae153372b417d3d63b28b385869af3e
CYA <3155414334@qq.com>
2025-10-29 19:45:14 +0800

    feat:新增了生成frame的功能

--- ros2_jazzy/src/chassis_driver/third_party/simple_frame/inc/simple_frame/simple_frame.hpp
+++ ros2_jazzy/src/chassis_driver/third_party/simple_frame/inc/simple_frame/simple_frame.hpp
@@ -28,14 +28,16 @@ private:
     //  数据包解析缓存区最大长度
     constexpr static uint16_t kParserBufferLength = 512;
     //  数据包包头指定
-    uint8_t packet_head_;
+    uint8_t frame_head_;
 public:
-    Simple_Frame_Parser( uint8_t packet_head );
-    void set_packet_head( uint8_t packet_head );
+    Simple_Frame_Parser( uint8_t frame_head );
+    void set_frame_head( uint8_t frame_head );
 
     bool get_command( lwrb_t* buff, uint8_t* data, uint16_t* size);  //  环形队列缓存的实际最大长度应该小于等于 kParserBufferLength
-    static bool pack_data(uint8_t* packet, uint8_t* data,uint16_t data_size, uint8_t packet_id);
-    static Simple_Frame_Parser::ParseState unpack_data(uint8_t* packet, uint8_t* data ,uint16_t* data_size,uint8_t packet_id);
+    uint16_t create_frame( uint8_t* frame, uint8_t* data,uint16_t data_size);
+
+    static uint16_t pack_data(uint8_t* frame, uint8_t* data,uint16_t data_size, uint8_t frame_id);
+    static Simple_Frame_Parser::ParseState unpack_data(uint8_t* frame, uint8_t* data ,uint16_t* data_size,uint8_t frame_id);
 }Simple_Frame;
 
 
--- ros2_jazzy/src/chassis_driver/third_party/simple_frame/src/simple_frame.cpp
+++ ros2_jazzy/src/chassis_driver/third_party/simple_frame/src/simple_frame.cpp
@@ -3,64 +3,64 @@
 namespace cya::hal::protocol
 {
 
-Simple_Frame_Parser::Simple_Frame_Parser( uint8_t packet_head )
-    : packet_head_( packet_head )
+Simple_Frame_Parser::Simple_Frame_Parser( uint8_t frame_head )
+    : frame_head_( frame_head )
 {}
 
-void Simple_Frame_Parser::set_packet_head( uint8_t packet_head )
+void Simple_Frame_Parser::set_frame_head( uint8_t frame_head )
 {
-    this->packet_head_ = packet_head;
+    this->frame_head_ = frame_head;
 }
 
-bool Simple_Frame_Parser::pack_data(uint8_t* packet, uint8_t* data,
-        uint16_t data_size,uint8_t packet_id)
+uint16_t Simple_Frame_Parser::pack_data(uint8_t* frame, uint8_t* data,
+        uint16_t data_size,uint8_t frame_id)
 {
     uint8_t sum = 0;
 
-    packet[0] = packet_id;
-    sum += packet[0];
+    frame[0] = frame_id;
+    sum += frame[0];
 
-    if( data_size + 3 > 256 )
-        return false;
+    if( data_size + 3 > Simple_Frame_Parser::kParserBufferLength )
+        return 0;
 
-    packet[1] = data_size + 3;
+    frame[1] = data_size + 3;
     
-    sum += packet[1];
+    sum += frame[1];
 
-    for( int i = 2 ; i < packet[1] - 1 ; ++i)
+    for( int i = 2 ; i < frame[1] - 1 ; ++i)
     {
-        packet[i] = data[ i - 2 ];
-        sum += packet[i];
+        frame[i] = data[ i - 2 ];
+        sum += frame[i];
     }
 
-    packet[ packet[1] - 1 ] = sum;
+    frame[ frame[1] - 1 ] = sum;
 
-    return true;
+    return frame[1];
 }
 
-Simple_Frame_Parser::ParseState Simple_Frame_Parser::unpack_data(uint8_t* packet, uint8_t* data ,uint16_t* size,uint8_t packet_id)
+Simple_Frame_Parser::ParseState Simple_Frame_Parser::unpack_data(uint8_t* frame, uint8_t* data ,uint16_t* size,uint8_t frame_id)
 {
-    if( packet_id != packet[0] )
+    if( frame_id != frame[0] )
         return Simple_Frame_Parser::ParseState::HEAD_BYTE_ERR;
 
-    uint16_t packet_size = packet[1];
+    uint16_t frame_size = frame[1];
 
-    if( packet_size < Simple_Frame_Parser::kCommandMinLength )
+    if( frame_size < Simple_Frame_Parser::kCommandMinLength )
         return Simple_Frame_Parser::ParseState::SIZE_BYTE_ERR;
     
     uint8_t sum = 0;
-    sum += packet[0];
-    sum += packet[1];
-    for( int i = 2 ; i < ( packet_size - 1 ) ; ++i )
+    sum += frame[0];
+    sum += frame[1];
+    for( int i = 2 ; i < ( frame_size - 1 ) ; ++i )
     {
-        data[i-2] = packet[i];
-        sum += packet[i];
+        data[i-2] = frame[i];
+        sum += frame[i];
     }
-    if( sum != packet[packet_size - 1] )
+    if( sum != frame[frame_size - 1] )
         return Simple_Frame_Parser::ParseState::CHECK_SUM_ERR;
 
     if( size )
-        *size = packet[1] - 3;
+        *size = frame[1] - 3;
 
     return Simple_Frame_Parser::ParseState::NO_ERR;
 }
@@ -68,26 +68,26 @@ Simple_Frame_Parser::ParseState Simple_Frame_Parser::unpack_data(uint8_t* packet
 bool Simple_Frame_Parser::get_command( lwrb_t* buff, uint8_t* data, uint16_t* size)
 {
     Simple_Frame_Parser::ParseState state = Simple_Frame_Parser::ParseState::NO_ERR;
-    uint16_t packet_size = 0;
+    uint16_t frame_size = 0;
     uint16_t skip_size = 0;
 
-    uint8_t packet[Simple_Frame_Parser::kParserBufferLength] = {0}; //  以后改为动态数组
+    uint8_t frame[Simple_Frame_Parser::kParserBufferLength] = {0}; //  以后改为动态数组
 
-    packet_size = lwrb_peek( buff, 0, packet, Simple_Frame_Parser::kParserBufferLength );
+    frame_size = lwrb_peek( buff, 0, frame, Simple_Frame_Parser::kParserBufferLength );
 
-    if( packet_size < Simple_Frame_Parser::kCommandMinLength )
+    if( frame_size < Simple_Frame_Parser::kCommandMinLength )
         return false;
 
     while( 1 )
     {
-        state = Simple_Frame_Parser::unpack_data( packet + skip_size, data, size,this->packet_head_ );
+        state = Simple_Frame_Parser::unpack_data( frame + skip_size, data, size,this->frame_head_ );
 
         if( state == Simple_Frame_Parser::ParseState::HEAD_BYTE_ERR ||
             state == Simple_Frame_Parser::ParseState::SIZE_BYTE_ERR ||
             state == Simple_Frame_Parser::ParseState::CHECK_SUM_ERR   )
         {
             ++skip_size;
-            if( ( packet_size - skip_size ) < Simple_Frame_Parser::kCommandMinLength )
+            if( ( frame_size - skip_size ) < Simple_Frame_Parser::kCommandMinLength )
             {
                 lwrb_skip( buff, skip_size);
                 return false;
@@ -95,10 +95,15 @@ bool Simple_Frame_Parser::get_command( lwrb_t* buff, uint8_t* data, uint16_t* si
                 continue;
             }
         } else if( state == Simple_Frame_Parser::ParseState::NO_ERR ){
-            lwrb_skip( buff,  skip_size + packet[skip_size+1] );
+            lwrb_skip( buff,  skip_size + frame[skip_size+1] );
             return true;
         }
     }
+}
+
+uint16_t Simple_Frame_Parser::create_frame( uint8_t* frame, uint8_t* data,uint16_t data_size)
+{
+    return this->pack_data( frame, data, data_size, this->frame_head_);
 }
 
 }
