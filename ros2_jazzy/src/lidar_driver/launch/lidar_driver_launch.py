bb471d80d47818f413cad16ca49f793c25a47152
CYA <3155414334@qq.com>
2025-11-01 20:56:51 +0800

    feat:引入LD14官方提供的库来处理通讯
    1. 引入了LD14官方驱动
    2. 引入了LD14官方Demo源文件
    3. 修改并配置了LD14官方提供的Launch文件
    4. 添加了少许注释

--- ros2_jazzy/src/lidar_driver/launch/lidar_driver_launch.py
+++ ros2_jazzy/src/lidar_driver/launch/lidar_driver_launch.py
@@ -26,10 +26,47 @@ def generate_launch_description():
 
     # 创建节点并且传入声明的参数
     lidar_node = Node(
-        package="lidar_driver",
-        executable="lidar_node",
+        package='lidar_driver',
+        executable='lidar_node',
+        name='ldlidar_publisher_ld14',
+        output='screen',
+        parameters=[
+            {'product_name': 'LDLiDAR_LD14'},
+            {'laser_scan_topic_name': 'scan'},
+            {'point_cloud_2d_topic_name': 'pointcloud2d'},
+            {'frame_id': 'laser_Link'},
+            {'port_name': '/dev/ttyVIRT1'},
+            {'serial_baudrate' : 115200},
+            {'laser_scan_dir': True},
+            {'enable_angle_crop_func': False},
+            {'angle_crop_min': 135.0},
+            {'angle_crop_max': 225.0}
+        ]
     )
+
     return LaunchDescription([
         colorized_output,
         lidar_node
     ])
+
+
+
+'''
+Parameter Description:
+---
+- Set laser scan directon: 
+  1. Set counterclockwise, example: {'laser_scan_dir': True}
+  2. Set clockwise,        example: {'laser_scan_dir': False}
+- Angle crop setting, Mask data within the set angle range:
+  1. Enable angle crop fuction:
+    1.1. enable angle crop,  example: {'enable_angle_crop_func': True}
+    1.2. disable angle crop, example: {'enable_angle_crop_func': False}
+  2. Angle cropping interval setting:
+  - The distance and intensity data within the set angle range will be set to 0.
+  - angle >= 'angle_crop_min' and angle <= 'angle_crop_max' which is [angle_crop_min, angle_crop_max], unit is degress.
+    example:
+      {'angle_crop_min': 135.0}
+      {'angle_crop_max': 225.0}
+      which is [135.0, 225.0], angle unit is degress.
+'''
+
---
+++ ros2_jazzy/src/lidar_driver/third_party/ld_lidar_driver/README.md
@@ -0,0 +1,10 @@
+# 第三方库来源
+**乐动机器人**公司的激光雷达传感器**LD14**开发手册
+[ROS2功能包 github仓库](https://github.com/ldrobotSensorTeam/ldlidar_sl_ros2.git)
+[Linux SDK github仓库](https://github.com/ldrobotSensorTeam/ldlidar_sl_sdk.git)
+
+# 第三方库介绍
+**乐动机器人**公司的激光雷达传感器**LD14**的驱动
+
+# 第三方库依赖
+待补充
